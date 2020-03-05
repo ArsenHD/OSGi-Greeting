85a38358d9711182f313044d48d6bced3f84ba48
unknown <nagoarsen@mail.ru>
2020-03-06 02:47:38 +0300

    Task 4 solution

---
+++ Greeting/HelloCommand/src/main/java/ru/softwerke/practice/hello/Hello.java
@@ -0,0 +1,20 @@
+package ru.softwerke.practice.hello;
+
+import org.apache.felix.scr.annotations.Component;
+import org.apache.felix.scr.annotations.Properties;
+import org.apache.felix.scr.annotations.Property;
+import org.apache.felix.scr.annotations.Service;
+
+@SuppressWarnings("deprecation")
+@Component
+@Service(value = Object.class)
+@Properties({
+        @Property(name = "osgi.command.scope", value = "practice"),
+        @Property(name = "osgi.command.function", value = "hello")
+})
+public class Hello {
+
+    public void hello(String param) {
+        System.out.println("Hello, " + param);
+    }
+}
